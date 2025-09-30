c9803f08e134333daaedf1eb37ef5bf2d46290b0
liyatomy-1030 <liyatomy26@gmail.com>
2025-09-30 22:22:00 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,24 @@
+print("welcome to my sample calculator")
+n=int(input("ENTER A NUMBER:"))
+m=int(input("ENTER ANOTHER NUMBER:"))
+k=int(input("ENTER A NUMBER FROM 1-4 for which 1- addition, 2-subraction,3- multiplication,4-divison:"))
+if (k==1)
+{
+    print("THE SUM OF THE NUMBER ",m+n)
+
+}
+if (k==2)
+{
+    print("THE DIFFERENCE OF THE NUMBER ",m-n)
+    
+}
+if (k==3)
+{
+    print("THE PRODUCT OF THE NUMBER ",m*n)
+    
+}
+if (k==4)
+{
+    print("THE RATIO OF THE NUMBER ",m//m)
+    
+}
